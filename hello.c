dd70272da07822a135d5f1c4d81e186db5f8459a
Tammy Nguyen <tnguyen152@huskers.unl.edu>
2020-09-04 16:28:33 +0000

    added my major to the output

---
+++ hello.c
@@ -0,0 +1,17 @@
+/**
+ * Author: Tammy Nguyen
+ * Date: 2020/09/02
+ *
+ * A simple hello world program in C
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+    printf("Hello World, I'm Tammy Nguyen!\n");
+    printf("My major is Computer Science!\n");
+
+    return 0;
+}
